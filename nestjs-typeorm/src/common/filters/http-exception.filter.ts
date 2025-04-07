ca4a36112ebe9bc93bfd7ae2b2c84e52a9b25a43
tiagosan040 <tiagosan040@gmail.com>
2025-04-07 15:50:52 -0300

    nestjs-typeorm: add custom http exception filter

---
+++ nestjs-typeorm/src/common/filters/http-exception.filter.ts
@@ -0,0 +1,32 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter<T extends HttpException>
+  implements ExceptionFilter
+{
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    const status = exception.getStatus();
+    const exceptionResponse = exception.getResponse();
+
+    const error =
+      typeof response === 'string'
+        ? { message: exceptionResponse }
+        : (exceptionResponse as object);
+
+    response.status(status).json({
+      ...error,
+      path: request.url,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
