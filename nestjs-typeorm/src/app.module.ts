d8e114a180aa53a7ef3dbc6a013a683310e25e59
tiagosan040 <tiagosan040@gmail.com>
2025-04-07 19:04:38 -0300

    nestjs-typeorm: remove unused module

--- nestjs-typeorm/src/app.module.ts
+++ nestjs-typeorm/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
@@ -13,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     CoursesModule,
     DatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
